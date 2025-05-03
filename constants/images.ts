aa8399220b8616449f77b631a4f507c9ef442e05
Andrew Sodeinde <22467921@stu.mmu.ac.uk>
2025-05-03 22:03:54 +0100

    Updates

--- constants/images.ts
+++
@@ -1,9 +0,0 @@
-import bg from "@/assets/images/bg.png"
-import highlight from "@/assets/images/highlight4.png"
-import rankingGradient from "@/assets/images/rankingGradient.png"
-
-export const images = {
-	bg,
-	highlight,
-	rankingGradient,
-}
